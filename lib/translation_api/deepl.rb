9f82278a1bd93bfcd4d07ebaded14d0bbf88fe49
milkeclair <milkeclair.black@gmail.com>
2024-11-23 03:35:58 +0900

    feat: 動作確認用にhello_worldを翻訳する処理を追加

--- lib/translation_api/deepl.rb
+++ lib/translation_api/deepl.rb
@@ -7,7 +7,26 @@ module TranslationAPI
   class DeepL
     def initialize
       Dotenv.load
-      raise "API key is not found" unless ENV["DEEPL_API_KEY"]
+      setup_deepl_config!
+      @supported_languages = supported_languages
+      source_lang = @supported_languages[:english]
+      target_lang = @supported_languages[:japanese]
+      p ::DeepL.translate("Hello, World!", source_lang, target_lang)
+    end
+
+    private
+
+    def setup_deepl_config!
+      raise "API key is not found" unless ENV["DEEPL_API_KEY"] || ENV["DEEPL_AUTH_KEY"]
+
+      ::DeepL.configure do |config|
+        config.auth_key = ENV["DEEPL_API_KEY"] || ENV["DEEPL_AUTH_KEY"]
+        config.host = "https://api-free.deepl.com"
+      end
+    end
+
+    def supported_languages
+      ::DeepL.languages.to_h { |lang| [lang.name.downcase.to_sym, lang.code] }
     end
   end
 end
