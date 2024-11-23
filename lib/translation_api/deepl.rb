67111b53a1e5d0f9a847c43a300a9a752470c55e
milkeclair <milkeclair.black@gmail.com>
2024-11-23 10:44:31 +0900

    feat: バリデーションを追加

--- lib/translation_api/deepl.rb
+++ lib/translation_api/deepl.rb
@@ -5,28 +5,45 @@ require "deepl"
 
 module TranslationAPI
   class DeepL
-    def initialize
+    def initialize(output_logs: true, except_words: [], language: "japanese", pro: false)
       Dotenv.load
-      setup_deepl_config!
-      @supported_languages = supported_languages
-      source_lang = @supported_languages[:english]
-      target_lang = @supported_languages[:japanese]
-      p ::DeepL.translate("Hello, World!", source_lang, target_lang)
+      setup_deepl_config!(pro: pro)
+      @supported_languages = fetch_supported_languages
+      validate_supported!(language)
+      @language = @supported_languages[language.to_sym]
+    end
+
+    def translate(text)
+      return text if text.strip.empty?
+
+      p ::DeepL.translate(text, nil, @language)
     end
 
     private
 
-    def setup_deepl_config!
-      raise "API key is not found" unless ENV["DEEPL_API_KEY"] || ENV["DEEPL_AUTH_KEY"]
+    def setup_deepl_config!(pro:)
+      validate_api_key!
 
       ::DeepL.configure do |config|
         config.auth_key = ENV["DEEPL_API_KEY"] || ENV["DEEPL_AUTH_KEY"]
-        config.host = "https://api-free.deepl.com"
+        config.host = pro ? "https://api.deepl.com" : "https://api-free.deepl.com"
       end
     end
 
-    def supported_languages
+    def validate_supported!(lang)
+      raise "This language is unsupported by DeepL" unless supported?(lang)
+    end
+
+    def validate_api_key!
+      raise "API key is not found" unless ENV["DEEPL_API_KEY"] || ENV["DEEPL_AUTH_KEY"]
+    end
+
+    def fetch_supported_languages
       ::DeepL.languages.to_h { |lang| [lang.name.downcase.to_sym, lang.code] }
+    end
+
+    def supported?(lang)
+      @supported_languages.key?(lang.to_sym)
     end
   end
 end
